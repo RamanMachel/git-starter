911179aa683e5da13c4bf9464fa47f1842501c2f
RamanMachel <raman.machel@gmail.com>
2021-09-23 22:00:36 +0300

    new_version

--- src/ru/machel/raman/Hello.java
+++ src/ru/machel/raman/Hello.java
@@ -2,6 +2,6 @@ package ru.machel.raman;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello GitHub");
+        System.out.println("Hello Grigoriy");
     }
 }
